e0708f577d34460c6a7122158eecb7f58f318273
Victor Liang <victor.zw.liang@gmail.com>
2020-07-22 04:06:32 +0000

    Added exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :apparel_type
+  
+def apparel_type
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must be true for at least one apparel type")
+      errors.add(:womens_apparel, "must be true for at least one apparel type")
+    end
+  end
 end
